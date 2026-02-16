d37d947e44b011ee55726b0cca1096e06611114b
Muhammad Apu <apu16-334@diu.edu.bd>
2026-02-16 12:48:00 +0600

    built mongodb atlas connection through mongoose

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
-const PORT = process.env.PORT;
 const app = require("./src/app.js");
+const connectDB = require("./src/config/db.js")
+
+connectDB()
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, ()=> console.log("server is running on the port: "+ PORT))
     .on("error", err => console.log('Server error:', err));
